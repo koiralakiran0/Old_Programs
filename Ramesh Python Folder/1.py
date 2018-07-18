fa579f6ed4bebad26fc11f0ef9e3f1a29a8e27c9
koiralakiran0 <kkoirala@uncc.edu>
2018-07-17 20:58:27 -0400

    added as backup

---
+++ Ramesh Python Folder/1.py
@@ -0,0 +1,12 @@
+name = input("Name: ")
+
+age = int(input("age: "))
+
+Ans = str((2018 - age)+100)
+
+print(" 100 years in " + Ans)
+
+many = int(input("how many times: "))
+print((int(many))*(" 100 years in " + Ans + "\n"))
+
+input("End")
