8f0b5cbce186fcb13071c7daa0b3ce8ae55d6ada
Ramesh Koirala <rkoirala@uncc.edu>
2020-06-13 01:43:56 -0400

    repeat float stuff

--- Java/ValidateFloat/floatCheck.java
+++ Java/ValidateFloat/floatCheck.java
@@ -1,8 +1,27 @@
 package ValidateFloat;
 
+import java.util.Scanner;
+
 public class floatCheck {
     public static void main(String[] args) {
-        String floatValue = "-5.2";
-        System.out.println( ValidateNumbers.validateFloat(floatValue) );
+        Scanner scanner = new Scanner(System.in);
+        boolean again = true;
+        String floatValue = null;
+        int i = 0;
+        while(again){
+            again = false;
+            System.out.print("Enter a value to check if it is a float: ");
+            floatValue = scanner.nextLine();
+            System.out.println( ValidateNumbers.validateFloat(floatValue) );
+
+            System.out.println("Would you like to go again? Enter 1 if you would like to go again");
+            i = scanner.nextInt();
+            scanner.nextLine();
+            if (i == 1){
+                again = true;
+            }
+        }
+
+        scanner.close();
     }
 }
